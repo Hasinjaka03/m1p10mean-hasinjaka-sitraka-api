313d7750f4f3493275de6977f379a0d230218147
HasinjakaKontiki <hasinjaka.kontiki@gmail.com>
2024-02-26 10:08:22 +0300

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/sallonbeautedb', {
+    // await mongoose.connect('mongodb://localhost:27017/sallonbeautedb', {
+    await mongoose.connect('mongodb+srv://hasinjaka:hasinjaka@cluster0.etzh1ga.mongodb.net/sallonbeautedb', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
